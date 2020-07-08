05e9ead1777c0155be573879a7680705b8c2da69
KTM28 <vabishya.gautam@gmail.com>
2020-07-08 13:34:50 +0100

    templates folder added and mongo data connection tested

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-#app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return f'<h1>Hello world!</h1>'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 
